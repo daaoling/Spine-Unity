e97d1ee6ef45d8cff17737525eea7225fd07662d
daaoling <1294246096@qq.com>
2015-11-02 23:12:26 +0800

    init

---
+++ Project/Assets/Editor/BuildSpine.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BuildSpine
+{
+    [MenuItem("Tools/BuildSpine")]
+    public static void DoIt()
+    {
+        string dirName = "Assets/Animation";
+        string spineFileName = "hounv";
+        string textureName = dirName + "/" + spineFileName + "/" + spineFileName + ".png";
+        string atlasFileName = dirName + "/" + spineFileName + "/" + spineFileName + ".atlas.txt";
+        string jsonFileName = dirName + "/" + spineFileName + "/" + spineFileName + ".json.txt";
+       
+
+        string atlasAssetName = dirName + "/" + spineFileName + "/" + spineFileName + ".asset";
+        string skeletonDataAssetName = dirName + "/" + spineFileName + "/" + spineFileName + "skeltonData" + ".asset";
+
+        ///1、 创建材质，并指贴图和shader
+        Shader shader = Shader.Find("Spine/SkeletonGhost");
+        Material mat = new Material(shader);
+        Texture tex = Resources.LoadAssetAtPath(textureName, typeof(Texture)) as Texture;
+        mat.SetTexture("_MainTex", tex);
+        AssetDatabase.CreateAsset(mat, dirName + "/" + spineFileName + "/" + spineFileName + ".mat");
+        AssetDatabase.SaveAssets();
+
+        ///2、 创建atlas，并指xx
+        AtlasAsset m_AtlasAsset = AtlasAsset.CreateInstance<AtlasAsset>();
+        AssetDatabase.CreateAsset(m_AtlasAsset, atlasAssetName);
+        Selection.activeObject = m_AtlasAsset;
+
+        TextAsset textAsset = Resources.LoadAssetAtPath(atlasFileName, typeof(TextAsset)) as TextAsset;
+        m_AtlasAsset.atlasFile = textAsset;
+        m_AtlasAsset.materials = new Material[1];
+        m_AtlasAsset.materials[0] = mat;
+        AssetDatabase.SaveAssets();
+
+        ///3、 创建SkeletonDataAsset，并指相关
+        SkeletonDataAsset m_skeltonDataAsset = SkeletonDataAsset.CreateInstance<SkeletonDataAsset>();
+        AssetDatabase.CreateAsset(m_skeltonDataAsset, skeletonDataAssetName);
+        Selection.activeObject = m_skeltonDataAsset;
+
+        m_skeltonDataAsset.atlasAssets = new AtlasAsset[1];
+        m_skeltonDataAsset.atlasAssets[0] = m_AtlasAsset;
+        TextAsset m_jsonAsset = Resources.LoadAssetAtPath(jsonFileName, typeof(TextAsset)) as TextAsset;
+        m_skeltonDataAsset.skeletonJSON = m_jsonAsset;
+        AssetDatabase.SaveAssets();
+
+
+        /// 创建场景物件
+        GameObject gameObject = new GameObject(spineFileName, typeof(SkeletonAnimation));
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = gameObject;
+
+        SkeletonAnimation m_skelAnim = gameObject.GetComponent<SkeletonAnimation>();
+        m_skelAnim.skeletonDataAsset = m_skeltonDataAsset;
+        //m_skelAnim.initialSkinName = "normal";
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Spine-Unity
-Spine-Unity
+#Spine 学习笔记
+
+# 目录结构:
+
+	/ComparePorject => [Wiki/Spine_Unity_Question Error Demo Project]
+
+
+
+
+# 随便记记
+
+[在Unity3D中使用Spine2D的骨骼动画](http://www.gameu3d.com/content/%E5%9C%A8unity3d%E4%B8%AD%E4%BD%BF%E7%94%A8spine2d%E7%9A%84%E9%AA%A8%E9%AA%BC%E5%8A%A8%E7%94%BB)
+
+
+[Spine输出资源一键入Unity3D工具代码](http://www.cnblogs.com/hellohuan/p/3926016.html)
+
+
+terminology
+notion
+spam
+junk
+elapse
+
+
+deselect => space
+
+vestigial
+
+https://www.youtube.com/playlist?list=PLwGl7Ikd_6GRFo7d0uRu_fN2RIlvkxW7b
